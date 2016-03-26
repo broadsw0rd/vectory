8c30316022aadd2a5538dfaa83641d1f21924637
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-03-26 15:45:42 +0300

    Tests for `Vector`

--- test/creation.test.js
+++ test/creation.test.js
@@ -1,6 +1,20 @@
 import Vector from '../dist/vectory.js'
 import test from 'ava'
 
-test('Vector should be a function', (t) => {
+test('`Vector` should be a function', (t) => {
   t.true(typeof Vector === 'function')
+})
+
+test('`new Vector(x, y)` should create vector instance', (t) => {
+  var vector = new Vector(1, 2)
+  t.true(vector instanceof Vector)
+  t.is(vector.x, 1)
+  t.is(vector.y, 2)
+})
+
+test('`new Vector(x, y)` should create zero vector if args not passed', (t) => {
+  var vector = new Vector()
+  t.true(vector instanceof Vector)
+  t.is(vector.x, 0)
+  t.is(vector.y, 0)
 })
