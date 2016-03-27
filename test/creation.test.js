cd92b870b2f89ecb91caeb21192a24a5f9632e59
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-03-27 09:49:05 +0300

    Tests for `Vector.from([x, u])`

--- test/creation.test.js
+++ test/creation.test.js
@@ -18,3 +18,14 @@ test('`new Vector(x, y)` should create zero vector if args not passed', (t) => {
   t.is(vector.x, 0)
   t.is(vector.y, 0)
 })
+
+test('`Vector.from` should be a function', (t) => {
+  t.true(typeof Vector.from === 'function')
+})
+
+test('`Vector.from([x, y])` should return new vector from data', (t) => {
+  var vector = Vector.from([1, 2])
+  t.true(vector instanceof Vector)
+  t.is(vector.x, 1)
+  t.is(vector.y, 2)
+})
