7106e156f4a05dd73ceab984fa918d916518ff97
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-03-27 10:18:47 +0300

    Tests for `Vector#add(vector)`

--- test/addition.test.js
+++ test/addition.test.js
@@ -13,3 +13,16 @@ test('`Vector.add(one, another)` should return sum of two vectors', (t) => {
   t.is(result.x, 4)
   t.is(result.y, 6)
 })
+
+test('`Vector#add` should be a function', (t) => {
+  t.true(typeof Vector.prototype.add === 'function')
+})
+
+test('`Vector#add(vector)` should return sum of two vectors', (t) => {
+  var self = new Vector(1, 2)
+  var vector = new Vector(3, 4)
+  var result = self.add(vector)
+  t.true(result instanceof Vector)
+  t.is(result.x, 4)
+  t.is(result.y, 6)
+})
