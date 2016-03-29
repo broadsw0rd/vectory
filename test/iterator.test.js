5cd45189513ea77d15c76c1c69af5ba447905ef4
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-03-29 16:54:45 +0300

    Restore support of Node 0.10; resolved #19

--- test/iterator.test.js
+++ test/iterator.test.js
@@ -1,12 +1,16 @@
 import Vector from '../dist/vectory.js'
 import test from 'ava'
 
-test('`Vector.prototype[Symbol.iterator]()` should return an iterator', (t) => {
-  var vector = new Vector(3, 4)
-  var iterator = vector[Symbol.iterator]()
-  t.ok(iterator)
-  t.true(typeof iterator.next === 'function')
-  t.same(iterator.next(), { done: false, value: 3 })
-  t.same(iterator.next(), { done: false, value: 4 })
-  t.same(iterator.next(), { done: true, value: void 0 })
-})
+if (typeof Symbol !== 'undefined' && Symbol.iterator) {
+  test('`Vector.prototype[Symbol.iterator]()` should return an iterator', (t) => {
+    var vector = new Vector(3, 4)
+    var iterator = vector[Symbol.iterator]()
+    t.ok(iterator)
+    t.true(typeof iterator.next === 'function')
+    t.same(iterator.next(), { done: false, value: 3 })
+    t.same(iterator.next(), { done: false, value: 4 })
+    t.same(iterator.next(), { done: true, value: void 0 })
+  })
+} else {
+  test.skip('`Vector.prototype[Symbol.iterator]()` should return an iterator', (t) => {})
+}
