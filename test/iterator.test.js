3d382dc4e2c18c59f8fe4e755458becffe15f037
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-05-07 11:16:44 +0300

    Remove deprecated ava methods

--- test/iterator.test.js
+++ test/iterator.test.js
@@ -5,11 +5,11 @@ if (typeof Symbol !== 'undefined' && Symbol.iterator && Vector.prototype[Symbol.
   test('`Vector.prototype[Symbol.iterator]()` should return an iterator', (t) => {
     var vector = new Vector(3, 4)
     var iterator = vector[Symbol.iterator]()
-    t.ok(iterator)
+    t.truthy(iterator)
     t.true(typeof iterator.next === 'function')
-    t.same(iterator.next(), { done: false, value: 3 })
-    t.same(iterator.next(), { done: false, value: 4 })
-    t.same(iterator.next(), { done: true, value: void 0 })
+    t.deepEqual(iterator.next(), { done: false, value: 3 })
+    t.deepEqual(iterator.next(), { done: false, value: 4 })
+    t.deepEqual(iterator.next(), { done: true, value: void 0 })
   })
 } else {
   test.skip('`Vector.prototype[Symbol.iterator]()` should return an iterator', (t) => {})
