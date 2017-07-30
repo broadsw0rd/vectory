1bc4bef7172361063d31e60db20154dfce19fe73
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-07-30 11:11:12 +0300

    Implemented filter for benchmarks; resolved #29

--- bench/index.js
+++ bench/index.js
@@ -1,33 +1,50 @@
 var Benchmark = require('benchmark')
 var logSymbols = require('log-symbols')
+var argv = require('minimist')(process.argv.slice(2))
 
-var benchmarks = {
-  'Cteation': require('./benchmarks/creation.bench.js'),
-  'Addition': require('./benchmarks/addition.bench.js'),
-  'Substraction': require('./benchmarks/substraction.bench.js'),
-  'Multiplication': require('./benchmarks/multiplication.bench.js'),
-  'Division': require('./benchmarks/division.bench.js'),
-  'Linear Interpolation': require('./benchmarks/lerp.bench.js'),
-  'Normalization': require('./benchmarks/normalization.bench.js'),
-  'Magnitude': require('./benchmarks/magnitude.bench.js'),
-  'Dot Product': require('./benchmarks/dot-product.bench.js'),
-  'Distance': require('./benchmarks/distance.bench.js'),
-  'Angle computing': require('./benchmarks/angle.bench.js'),
-  'Resetting': require('./benchmarks/resetting.bench.js'),
-  'Copy': require('./benchmarks/copy.bench.js'),
-  'Convertion': require('./benchmarks/convertion.bench.js'),
-  'Equality': require('./benchmarks/equality.bench.js'),
-  'Swizzling': require('./benchmarks/swizzling.bench.js'),
-  'Iterator': require('./benchmarks/iterator.bench.js')
+var titles = {
+  'create': 'Cteation',
+  'add': 'Addition',
+  'sub': 'Substraction',
+  'mul': 'Multiplication',
+  'div': 'Division',
+  'lerp': 'Linear Interpolation',
+  'norm': 'Normalization',
+  'mag': 'Magnitude',
+  'dot': 'Dot Product',
+  'dist': 'Distance',
+  'angle': 'Angle computing',
+  'reset': 'Resetting',
+  'copy': 'Copy',
+  'convert': 'Convertion',
+  'equal': 'Equality',
+  'swizzling': 'Swizzling',
+  'iterator': 'Iterator'
 }
 
-var keys = Object.keys(benchmarks)
+var benchmarks = {
+  'create': require('./benchmarks/creation.bench.js'),
+  'add': require('./benchmarks/addition.bench.js'),
+  'sub': require('./benchmarks/substraction.bench.js'),
+  'mul': require('./benchmarks/multiplication.bench.js'),
+  'dev': require('./benchmarks/division.bench.js'),
+  'lerp': require('./benchmarks/lerp.bench.js'),
+  'norm': require('./benchmarks/normalization.bench.js'),
+  'mag': require('./benchmarks/magnitude.bench.js'),
+  'dot': require('./benchmarks/dot-product.bench.js'),
+  'dist': require('./benchmarks/distance.bench.js'),
+  'angle': require('./benchmarks/angle.bench.js'),
+  'reset': require('./benchmarks/resetting.bench.js'),
+  'copy': require('./benchmarks/copy.bench.js'),
+  'convert': require('./benchmarks/convertion.bench.js'),
+  'equal': require('./benchmarks/equality.bench.js'),
+  'swizzling': require('./benchmarks/swizzling.bench.js'),
+  'iterator': require('./benchmarks/iterator.bench.js')
+}
 
-console.log(logSymbols.info + ' start benchmark, please wait a bit...' + '\r\n')
-
-for (var i = 0; i < keys.length; i++) {
-  var title = keys[i]
-  var benchmark = benchmarks[title]
+function bench (name) {
+  var title = titles[name]
+  var benchmark = benchmarks[name]
   var suite = new Benchmark.Suite()
   var body = []
 
@@ -46,4 +63,17 @@ for (var i = 0; i < keys.length; i++) {
   suite.run()
 
   console.log(body.join('\r\n') + '\r\n')
+}
+
+console.log(logSymbols.info + ' start benchmark, please wait a bit...' + '\r\n')
+
+var filter = argv.filter
+
+if (filter) {
+  bench(filter)
+} else {
+  var keys = Object.keys(benchmarks)
+  for (var i = 0; i < keys.length; i++) {
+    bench(keys[i])
+  }
 }
