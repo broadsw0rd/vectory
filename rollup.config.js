53f6451e09913ee448d40abda38819724f4e43aa
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-09-04 09:36:08 +0300

    Update version, fixed lint errors

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,8 @@ import commonjs from 'rollup-plugin-commonjs'
 export default {
   input: 'src/index.js',
   output: [
-  	{ file: pkg.main, format: 'umd', name: 'Vector' },
-  	{ file: pkg.module, format: 'es' }
+    { file: pkg.main, format: 'umd', name: 'Vector' },
+    { file: pkg.module, format: 'es' }
   ],
   plugins: [
     nodeResolve(),
