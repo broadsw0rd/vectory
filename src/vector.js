8a46125e45f1a2c7678f28f7e5accee8aef14eeb
Vladimir Bykov <broadsw.rd@yandex.ru>
2018-03-10 10:41:09 +0300

    JSDoc usage

--- src/vector.js
+++ src/vector.js
@@ -1,3 +1,11 @@
+/**
+ * Vector constructor
+ * @constructs
+ * @param {number} x First vector component
+ * @param {number} y Second vector component
+ * @example
+ * var vector = new Vector(10, 20)
+ */
 function Vector (x, y) {
   this.x = x || 0
   this.y = y || 0
